b9d33efa21868fbdf2a036207a5eda16c11035a0
Adri1FASST <adrien.nombalier@fasst.io>
2020-11-18 14:16:02 +0100

    User registration & validation done

--- HERMES/Chat/graphql/resolvers.js
+++ HERMES/Chat/graphql/resolvers.js
@@ -10,7 +10,7 @@ module.exports = {
         const users = await User.findAll();
         return users;
       } catch (err) {
-        console.log(err);
+        console.error(err);
       }
     },
   },
@@ -19,7 +19,7 @@ module.exports = {
       let { username, email, password, confirmPassword } = args;
       let errors = {};
       try {
-        // TODO validate input data
+        // Validate input data
         if (email.trim() === "") errors.email = "Email must not be empty";
         if (username.trim() === "")
           errors.username = "Username must not be empty";
@@ -27,32 +27,37 @@ module.exports = {
           errors.password = "Password must not be empty";
         if (confirmPassword.trim() === "")
           errors.confirmPassword = "Repeat must not be empty";
-
         if (password !== confirmPassword)
           errors.confirmPassword = "passwords must match";
-        // TODO Check if username …
-        const userByUsername = await User.findOne({ where: { username } });
-        const userByEmail = await User.findOne({ where: { email } });
 
-        if (userByUsername) errors.username = "Username is taken";
-        if (userByEmail) errors.email = "Email is taken";
+        // Check username or email already exist
+        // const userByUsername = await User.findOne({ where: { username } });
+        // const userByEmail = await User.findOne({ where: { email } });
+        // if (userByUsername) errors.username = "Username is taken";
+        // if (userByEmail) errors.email = "Email is taken";
 
         if (Object.keys(errors).length > 0) {
           throw errors;
         }
         // Hash password
         password = await bcrypt.hash(password, 6);
-        // TODO Create user
+        // Create user
         const user = await User.create({
           username,
           email,
           password,
         });
-        // TODO Return user
+        // Return user
         return user;
       } catch (err) {
-        console.log(err);
-        throw new UserInputError("Bad input", { errors: err });
+        if (err.name === "SequelizeUniqueConstraintError") {
+          err.errors.forEach(
+            (e) => (errors[e.path] = `${e.path} already taken`)
+          );
+        } else if (err.name === "SequelizeValidationError") {
+          err.errors.forEach((e) => (errors[e.path] = e.message));
+        }
+        throw new UserInputError("Bad input", { errors });
       }
     },
   },
