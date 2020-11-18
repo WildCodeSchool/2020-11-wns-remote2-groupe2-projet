30c38c7c4990353fb448f796295f4776de3de5fe
Adri1FASST <adrien.nombalier@fasst.io>
2020-11-18 11:32:38 +0100

    add .env on index sequelize

---
+++ HERMES/Chat/config/config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    operatorsAliases: 1,
+  },
+  test: {
+    use_env_variable: "DATABASE_URL",
+    operatorsAliases: 1,
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    operatorsAliases: 1,
+  },
+};
