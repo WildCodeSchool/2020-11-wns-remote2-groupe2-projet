3d1080cb490bb837936c149708eae1a650a3e616
moshtaghfr <moshtagh.fr@gmail.com>
2020-11-20 12:39:08 +0100

    create registration form and connected to ddb

--- HERMES/Chat/client/src/App.js
+++ HERMES/Chat/client/src/App.js
@@ -1,41 +1,14 @@
 import React from 'react';
-import { Container, Row , Col , Form , Button} from 'react-bootstrap';
+import { Container} from 'react-bootstrap';
 import './App.scss';
+import Register from "./pages/Register";
 
 function App() {
+
+
   return (
     <Container className="pt-5">
-      <Row className="bg-white py-5 justify-content-center">
-        <Col sm={8} md={6} lg={4}>
-        <h1 className="text-center">
-        Inscription
-      </h1>
-      <Form >
-  <Form.Group >
-    <Form.Label>username</Form.Label>
-    <Form.Control type="text"  />
-    </Form.Group>
-    <Form.Group >
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email"  />
-    </Form.Group>
-    <Form.Group >
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password"  />
-    </Form.Group>
-    <Form.Group >
-    <Form.Label>Confirmation Password</Form.Label>
-    <Form.Control type="confirmationPassword"  />
-    </Form.Group>
-    <div className="text-center">
-    <Button variant="success" type="submit">
-    Enregistrer
-    </Button>
-    </div>
-  
-</Form>
-        </Col>
-      </Row>
+     <Register />
     </Container>
    
   );
