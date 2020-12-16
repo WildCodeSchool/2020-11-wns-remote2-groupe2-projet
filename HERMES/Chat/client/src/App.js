2d9a1afb1f2178fa6dbeab28bf01f74c7ac3d780
Bwouno <bwouno@icloud.com>
2020-12-16 17:20:39 +0100

    video 6

--- HERMES/Chat/client/src/App.js
+++ HERMES/Chat/client/src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import {Container} from 'react-bootstrap';
 import './App.scss';
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 import ApolloProvider from "./ApolloProvider";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <ApolloProvider>
+            <BrowserRouter>
             <Container className="pt-5">
-                <Register/>
+                <Switch>
+                <Route exact path={"/"} component={Home} />
+                <Route path={"/login"} component={Login} />
+                <Route path={"/register"} component={Register} />
+                </Switch>
             </Container>
+            </BrowserRouter>
         </ApolloProvider>
     );
 }
