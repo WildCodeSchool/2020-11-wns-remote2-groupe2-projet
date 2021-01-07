4f5ba68765a066e3f0c92bae66a460a5671e13ec
lefernix <a.nombalier@gmail.com>
2021-01-07 16:16:58 +0100

    fix  bg-secondary color

--- HERMES/Chat/client/src/pages/Home.js
+++ HERMES/Chat/client/src/pages/Home.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, Fragment } from 'react'
-import { Row, Col, Button, Image } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { gql, useQuery, useLazyQuery } from '@apollo/client'
-import { useAuthDispatch } from '../context/auth'
+import React, { useEffect, useState, Fragment } from "react";
+import { Row, Col, Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { gql, useQuery, useLazyQuery } from "@apollo/client";
+import { useAuthDispatch } from "../context/auth";
 const GET_USERS = gql`
   query getUsers {
     getUsers {
@@ -18,7 +18,7 @@ const GET_USERS = gql`
       }
     }
   }
-`
+`;
 const GET_MESSAGES = gql`
   query getMessages($from: String!) {
     getMessages(from: $from) {
@@ -29,65 +29,81 @@ const GET_MESSAGES = gql`
       createdAt
     }
   }
-`
-export default function Home ({ history }) {
-  const dispatch = useAuthDispatch()
-  const [selectedUser, setSelectedUser] = useState(null)
+`;
+export default function Home({ history }) {
+  const dispatch = useAuthDispatch();
+  const [selectedUser, setSelectedUser] = useState(null);
   const logout = () => {
-    dispatch({ type: "LOGOUT" })
-    history.push("/login")
-  }
-  const { loading, data, error } = useQuery(GET_USERS)
-  const [getMessages, { loading: messagesLoading, data: messagesData }] = useLazyQuery(GET_MESSAGES)
+    dispatch({ type: "LOGOUT" });
+    history.push("/login");
+  };
+  const { loading, data, error } = useQuery(GET_USERS);
+  const [
+    getMessages,
+    { loading: messagesLoading, data: messagesData },
+  ] = useLazyQuery(GET_MESSAGES);
   useEffect(() => {
-    if(selectedUser) {
-      getMessages({ variables: { from: selectedUser } })
+    if (selectedUser) {
+      getMessages({ variables: { from: selectedUser } });
     }
-  }, [selectedUser])
-  if(messagesData) console.log(messagesData.getMessages)
-  let usersMarkup
+  }, [selectedUser]);
+  if (messagesData) console.log(messagesData.getMessages);
+  let usersMarkup;
   if (!data || loading) {
-    usersMarkup = <p>Loading..</p>
+    usersMarkup = <p>Loading..</p>;
   } else if (data.getUsers.length === 0) {
-    usersMarkup = <p>No users have joined yet</p>
+    usersMarkup = <p>No users have joined yet</p>;
   } else if (data.getUsers.length > 0) {
     usersMarkup = data.getUsers.map((user) => (
-      <div className='d-flex p-3' key={user.username} onClick={() => setSelectedUser(user.username)}>
-        <Image src={user.imageUrl} roundedCircle className='mr-2' style={{ width: 50, height: 50, objectFit: 'cover' }} />
+      <div
+        className="d-flex p-3"
+        key={user.username}
+        onClick={() => setSelectedUser(user.username)}
+      >
+        <Image
+          src={user.imageUrl}
+          roundedCircle
+          className="mr-2"
+          style={{ width: 50, height: 50, objectFit: "cover" }}
+        />
         <div>
-          <p className='text-success'>{user.username}</p>
-          <p className='font-weight-light'>
+          <p className="text-success">{user.username}</p>
+          <p className="font-weight-light">
             {user.latestMessage
               ? user.latestMessage.content
               : "You are now connected!"}
           </p>
         </div>
       </div>
-    ))
+    ));
   }
   return (
     <>
-      <Row className='bg-white justify-content-around mb-1'>
-        <Link to='/login'>
-          <Button variant='link'>Login</Button>
+      <Row className="bg-white justify-content-around mb-1">
+        <Link to="/login">
+          <Button variant="link">Login</Button>
         </Link>
-        <Link to='/register'>
-          <Button variant='link'>Register</Button>
+        <Link to="/register">
+          <Button variant="link">Register</Button>
         </Link>
-        <Button variant='link' onClick={logout}>
+        <Button variant="link" onClick={logout}>
           Logout
         </Button>
       </Row>
-      <Row className='bg-white'>
-        <Col xs={4} className='p-0'>{usersMarkup}</Col>
+      <Row className="bg-white">
+        <Col xs={4} className="p-0 bg-secondary">
+          {usersMarkup}
+        </Col>
         <Col xs={8}>
           {messagesData && messagesData.getMessages.length > 0 ? (
-            messagesData.getMessages.map(message => (
+            messagesData.getMessages.map((message) => (
               <p key={message.uuid}>{message.content}</p>
             ))
-          ) : (<p>Messages</p>)}
+          ) : (
+            <p>Messages</p>
+          )}
         </Col>
       </Row>
     </>
-  )
+  );
 }
