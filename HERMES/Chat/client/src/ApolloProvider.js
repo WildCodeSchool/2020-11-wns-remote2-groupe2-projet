7d1a9f4f1dd23a271bf5ae1cb71cfc13bcfddd9b
Bwouno <bwouno@icloud.com>
2021-01-31 13:56:02 +0100

    video 13 and 14

--- HERMES/Chat/client/src/ApolloProvider.js
+++ HERMES/Chat/client/src/ApolloProvider.js
@@ -1,29 +1,61 @@
-import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider as Provider, createHttpLink } from '@apollo/client';
+import React from "react";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider as Provider,
+  createHttpLink,
+  split
+} from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { setContext } from "@apollo/client/link/context";
+import { getMainDefinition } from '@apollo/client/utilities';
 
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+let httpLink = createHttpLink({
+	uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem("token");
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
+
+httpLink = authLink.concat(httpLink)
+
+
+const wsLink = new WebSocketLink({
+	uri: `ws://localhost:4000/graphql`,
+	options: {
+		reconnect: true,
+		connectionParams: {
+			Authorization: `Bearer ${localStorage.getItem("token")}`,
+		},
+	},
+});
+
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return (
+			definition.kind === "OperationDefinition" &&
+			definition.operation === "subscription"
+		);
+	},
+	wsLink,
+	httpLink,
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+	link: splitLink,
+	cache: new InMemoryCache(),
 });
 
 export default function ApolloProvider(props) {
-    return <Provider client={client} {...props} />
+	return <Provider client={client} {...props} />;
 }
--- HERMES/Chat/client/src/pages/home/Messages.js
+++ HERMES/Chat/client/src/pages/home/Messages.js
@@ -1,117 +1,124 @@
-import React, { useEffect, Fragment, useState } from 'react'
-import { gql, useLazyQuery, useMutation } from '@apollo/client'
-import { Col, Form } from 'react-bootstrap'
+import React, { useEffect, Fragment, useState } from "react";
+import { gql, useLazyQuery, useMutation } from "@apollo/client";
+import { Col, Form } from "react-bootstrap";
 
-import { useMessageDispatch, useMessageState } from '../../context/message'
+import { useMessageDispatch, useMessageState } from "../../context/message";
 
-import Message from './Message'
+import Message from "./Message";
 
 const SEND_MESSAGE = gql`
-  mutation sendMessage($to: String!, $content: String!){
-    sendMessage(to: $to, content: $content){
-    uuid from to content createdAt
-    }
-  }
-`
+	mutation sendMessage($to: String!, $content: String!) {
+		sendMessage(to: $to, content: $content) {
+			uuid
+			from
+			to
+			content
+			createdAt
+		}
+	}
+`;
 
 const GET_MESSAGES = gql`
-  query getMessages($from: String!) {
-    getMessages(from: $from) {
-      uuid
-      from
-      to
-      content
-      createdAt
-    }
-  }
-`
+	query getMessages($from: String!) {
+		getMessages(from: $from) {
+			uuid
+			from
+			to
+			content
+			createdAt
+		}
+	}
+`;
 
-export default function Messages () {
-  const { users } = useMessageState()
-  const dispatch = useMessageDispatch()
-  const [content, setContent] = useState('')
+export default function Messages() {
+	const { users } = useMessageState();
+	const dispatch = useMessageDispatch();
+	const [content, setContent] = useState("");
 
-  const selectedUser = users?.find((u) => u.selected === true)
-  const messages = selectedUser?.messages
-  
-  const [getMessages, { loading: messagesLoading, data: messagesData },] = useLazyQuery(GET_MESSAGES)
+	const selectedUser = users?.find((u) => u.selected === true);
+	const messages = selectedUser?.messages;
 
-  const [sendMessage] = useMutation(SEND_MESSAGE, {
-    onCompleted: (data) => 
-      dispatch({ 
-        type: 'ADD_MESSAGE', 
-        payload: {
-          username: selectedUser.username,
-          message: data.sendMessage
-        },
-      }),
-    onError: (err) => console.log(err),
-  })
+	const [
+		getMessages,
+		{ loading: messagesLoading, data: messagesData },
+	] = useLazyQuery(GET_MESSAGES);
 
-  useEffect(() => {
-    if (selectedUser && !selectedUser.messages) {
-      getMessages({ variables: { from: selectedUser.username } })
-    }
-  }, [selectedUser])
+	const [sendMessage] = useMutation(SEND_MESSAGE, {
+		onError: (err) => console.log(err),
+	});
 
-  useEffect(() => {
-      if(messagesData){
-          dispatch({ type: 'SET_USER_MESSAGES', payload: { 
-              username: selectedUser.username, 
-              messages: messagesData.getMessages
-          } })
-      }
-  }, [messagesData])
+	useEffect(() => {
+		if (selectedUser && !selectedUser.messages) {
+			getMessages({ variables: { from: selectedUser.username } });
+		}
+	}, [selectedUser]);
 
-  const submitMessage = (e) => {
-    e.preventDefault()
-    if(content.trim() === '' || !selectedUser) return
+	useEffect(() => {
+		if (messagesData) {
+			dispatch({
+				type: "SET_USER_MESSAGES",
+				payload: {
+					username: selectedUser.username,
+					messages: messagesData.getMessages,
+				},
+			});
+		}
+	}, [messagesData]);
 
-    setContent('')
+	const submitMessage = (e) => {
+		e.preventDefault();
+		if (content.trim() === "" || !selectedUser) return;
 
-    // Mutation for sending the message
-    sendMessage({ variables: { to: selectedUser.username, content } })
-  }
+		setContent("");
 
-  let selectedChatMarkup
-  if(!messages && !messagesLoading){
-    selectedChatMarkup = <p className='info-text'>Selectionnez un contact</p>
-  } else if(messagesLoading) {
-    selectedChatMarkup = <p className='info-text'>Loading</p>
-  } else if(messages.length > 0) {
-    selectedChatMarkup = messages.map((message, index) => (
-        <Fragment key={message.uuid}>
-            <Message message={message} />
-            {index === messages.length - 1 && (
-                <div className="invisible">
-                    <hr  className='m-0'/>
-                </div>
-            )}
-        </Fragment>
-    ))
-  } else if(messages.length ===0){
-    selectedChatMarkup = <p>Vous êtes connecté(e), envoyer un message .. </p>
-  }
+		// Mutation for sending the message
+		sendMessage({ variables: { to: selectedUser.username, content } });
+	};
 
-  return (
-    <Col xs={10} md={8}>
-      <div className='messages-box d-flex flex-column-reverse'>
-        {selectedChatMarkup}
-      </div>
-      <div>
-        <Form onSubmit={submitMessage}>
-          <Form.Group className="d-flex align-items-center">
-            <Form.Control
-              type='formtext'
-              className='message-intput rounded-pill p-4 bg-secondary border-0'
-              placeholder='Entrer un message ..'
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
-            <i className="fas fa-paper-plane fa-2x text-primary ml-2" onClick={submitMessage}> </i>
-          </Form.Group>
-        </Form>
-      </div>
-    </Col>
-  )
+	let selectedChatMarkup;
+	if (!messages && !messagesLoading) {
+		selectedChatMarkup = <p className="info-text">Selectionnez un contact</p>;
+	} else if (messagesLoading) {
+		selectedChatMarkup = <p className="info-text">Loading</p>;
+	} else if (messages.length > 0) {
+		selectedChatMarkup = messages.map((message, index) => (
+			<Fragment key={message.uuid}>
+				<Message message={message} />
+				{index === messages.length - 1 && (
+					<div className="invisible">
+						<hr className="m-0" />
+					</div>
+				)}
+			</Fragment>
+		));
+	} else if (messages.length === 0) {
+		selectedChatMarkup = <p>Vous êtes connecté(e), envoyer un message .. </p>;
+	}
+
+	return (
+		<Col xs={10} md={8}>
+			<div className="messages-box d-flex flex-column-reverse">
+				{selectedChatMarkup}
+			</div>
+			<div>
+				<Form onSubmit={submitMessage}>
+					<Form.Group className="d-flex align-items-center">
+						<Form.Control
+							type="formtext"
+							className="message-intput rounded-pill p-4 bg-secondary border-0"
+							placeholder="Entrer un message .."
+							value={content}
+							onChange={(e) => setContent(e.target.value)}
+						/>
+						<i
+							className="fas fa-paper-plane fa-2x text-primary ml-2"
+							onClick={submitMessage}
+						>
+							{" "}
+						</i>
+					</Form.Group>
+				</Form>
+			</div>
+		</Col>
+	);
 }
