53038c865bcb493dd6ebc399e7a65433719d06f9
Victor Martin <victor.martin@domicours.fr>
2021-04-07 16:31:47 +0200

    pour Moshtagh

--- HERMES/Chat/client/src/pages/Register.js
+++ HERMES/Chat/client/src/pages/Register.js
@@ -35,7 +35,7 @@ export default function Register(props) {
 
   const [registerUser, { loading }] = useMutation(REGISTER_USER, {
     update: (_, __) => props.history.push("/login"),
-    onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
+    onError: (err) => {console.log("err" + err);(setErrors(err.graphQLErrors[0].extensions.errors))},
   });
 
   const submitRegisterForm = (e) => {
