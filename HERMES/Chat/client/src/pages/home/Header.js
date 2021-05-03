e01fbce08a22ec02d8f71020d19501dc9f506b52
lefernix <adri1n.apple@gmail.com>
2021-05-03 20:05:48 +0200

    start tabs

--- HERMES/Chat/client/src/pages/home/Header.js
+++ HERMES/Chat/client/src/pages/home/Header.js
@@ -4,6 +4,7 @@ import { FormControl, FormLabel } from '@chakra-ui/form-control'
 import { useDisclosure } from '@chakra-ui/hooks'
 import { Input } from '@chakra-ui/input'
 import { Badge, Box, Flex, Text } from '@chakra-ui/layout'
+import { Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList } from '@chakra-ui/menu'
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/modal'
 import React from 'react'
 import { useAuthDispatch } from '../../context/auth'
@@ -11,14 +12,11 @@ import { useMessageState } from '../../context/message'
 
 export default function Header() {
     const authDispatch = useAuthDispatch();
-    const { isOpen: isOpenUpdateInformation, onOpen: onOpenUpdateInformation, onClose: onCloseUpdateInformation } = useDisclosure()
     const { isOpen: isOpenDisconnect, onOpen: onOpenDisconnect, onClose: onCloseDisconnect } = useDisclosure()
 
     const { user } = useMessageState();
 
     const baseURL = process.env.REACT_APP_BASE_URL || "";
-    const initialRef = React.useRef()
-    const finalRef = React.useRef()
 
     const logout = () => {
         authDispatch({ type: "LOGOUT" });
@@ -29,10 +27,8 @@ export default function Header() {
             display="flex"
             justifyContent="space-between"
             alignItems="center"
+            flexDirection="row-reverse"
             m={3}>
-            <Button
-                onClick={onOpenDisconnect}
-            >Se déconnecter</Button>
             <Modal
                 isCentered
                 onClose={onCloseDisconnect}
@@ -56,39 +52,27 @@ export default function Header() {
                     </Text>
                     <Text fontSize="sm" color="white">Developer</Text>
                 </Box>
-                <Avatar cursor="pointer" onClick={onOpenUpdateInformation} loading="eager" m={1} src={baseURL + user?.imageUrl} >
-                    <AvatarBadge borderColor="#39414f" boxSize="0.80em" bg="green.500" />
-                </Avatar>
-                <Modal
-                    initialFocusRef={initialRef}
-                    finalFocusRef={finalRef}
-                    isOpen={isOpenUpdateInformation}
-                    onClose={onCloseUpdateInformation}
-                >
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader>Compte</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody pb={6}>
-                            <FormControl>
-                                <FormLabel>Nom d'utilisateur</FormLabel>
-                                <Input ref={initialRef} value={user?.username} />
-                            </FormControl>
-                            {/* RAJOUTER CHAMP CAMPUS */}
-                            {/* RAJOUTER CHAMP DESCRIPTION ? */}
-                            <FormControl mt={4}>
-                                <FormLabel>Email</FormLabel>
-                                <Input value={user?.email} />
-                            </FormControl>
-                        </ModalBody>
 
-                        <ModalFooter>
-                            <Button colorScheme="blue" mr={3}>
-                                Enregister</Button>
-                            <Button onClick={onCloseUpdateInformation}>Retour</Button>
-                        </ModalFooter>
-                    </ModalContent>
-                </Modal>
+                <Menu>
+                    <MenuButton cursor="pointer" as={Avatar} colorScheme="pink">
+                        <Avatar loading="eager" m={1} src={baseURL + user?.imageUrl} >
+                            <AvatarBadge borderColor="#39414f" boxSize="0.80em" bg="green.500" />
+                        </Avatar>
+                    </MenuButton>
+                    <MenuList>
+                        <MenuGroup title="Profil">
+                            <MenuItem>Mon compte</MenuItem>
+                            <MenuItem>Ma classe </MenuItem>
+                        </MenuGroup>
+                        <MenuDivider />
+                        <MenuGroup title="Paramètres">
+                            <MenuItem>Docs</MenuItem>
+                            <MenuItem>FAQ</MenuItem>
+                        </MenuGroup>
+                        <MenuDivider />
+                        <Button onClick={onOpenDisconnect}>Deconnexion</Button>
+                    </MenuList>
+                </Menu>
             </Flex>
         </Box>
 
